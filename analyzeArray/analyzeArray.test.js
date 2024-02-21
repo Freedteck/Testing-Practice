1be3ef9f39a4a0b72c150dd7b59f92a0dcbad66d
Freedteck <freedteck@gmail.com>
2024-02-21 19:49:33 +0100

    remove node_modules

--- analyzeArray/analyzeArray.test.js
+++
@@ -1,5 +0,0 @@
-import analyzeArray from "./analyzeArray";
-
-test(`Array [1,8,3,4,2,6] is analysed to {average: 4, min: 1, max: 8,length: 6};`, () => {
-    expect(analyzeArray([1,8,3,4,2,6])).toEqual({average: 4, min: 1, max: 8, length: 6})
-  })
